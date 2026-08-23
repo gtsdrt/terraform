5a5572c304168121af1bef8e96439daf1b6dbb7a
gtsdrt <gtsdrt@gmail.com>
2026-08-23 16:07:19 +0200

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,4 +1,6 @@
-resource "azurerm_resource_group" "demo" {
-  name     = "terraform-executor-demo"
-  location = "norwayeast"
+resource "azurerm_virtual_network" "demo" {
+  name                = "terraform-executor-demo-vnet"
+  location            = "norwayeast"
+  resource_group_name = "terraform"
+  address_space       = ["10.42.0.0/16"]
 }
